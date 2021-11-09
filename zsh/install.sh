3842c2b57dffd256fe976ecb87e44cdba7a81eda
Phil Haack <haacked@gmail.com>
2021-11-09 11:51:39 -0800

    Add a .bash_exports file to my zsh config

---
+++ zsh/install.sh
@@ -0,0 +1,6 @@
+if [ -f $HOME/.iterm2_shell_integration.zsh ]; then
+    echo "iTerm2 shell integration already installed."
+else
+    echo "Installing iTerm2 shell integration..."
+    curl -L https://iterm2.com/shell_integration/zsh -o $HOME/.iterm2_shell_integration.zsh
+fi
