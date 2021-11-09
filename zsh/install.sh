8a442a6d6e2f262e51ad4238accd4f8b72fb18ce
Phil Haack <haacked@gmail.com>
2021-11-09 11:56:28 -0800

    Add jetbrains-toolbox and postman to my install list

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,3 +4,5 @@ else
     echo "Installing iTerm2 shell integration..."
     curl -L https://iterm2.com/shell_integration/zsh -o $HOME/.iterm2_shell_integration.zsh
 fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
