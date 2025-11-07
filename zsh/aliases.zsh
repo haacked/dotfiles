7fa5d42cd8db29ebab881ba3c1bcb0bc03ffb5fe
Phil Haack <haacked@gmail.com>
2025-11-07 11:28:02 -0800

    Add disk space aliases for better usability
    - disk-free: Show just available space in GB
    - disk-usage: Show only main filesystems (cleaner output)
    - disk-usage-all: Show all filesystems (previous behavior)
    - disk-cleanup-*: Use new modular cleanup system with --module flags

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,9 @@ alias pytest-changes='git bchanges | grep "test_[^/]*\.py$" | xargs pytest'
 
 # Disk Space Management
 alias disk-check='~/.dotfiles/bin/check-disk-space'
-alias disk-usage='df -h'
+alias disk-free='df -h /System/Volumes/Data | tail -1 | awk "{print \$4}"'
+alias disk-usage='df -h / /System/Volumes/Data | grep -v "^Filesystem"'
+alias disk-usage-all='df -h'
 alias disk-cleanup='~/.dotfiles/bin/disk-cleanup'
 alias disk-cleanup-aggressive='~/.dotfiles/bin/disk-cleanup --aggressive'
 alias disk-cleanup-docker='~/.dotfiles/bin/disk-cleanup --module docker'
